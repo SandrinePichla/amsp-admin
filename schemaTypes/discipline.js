c82e4848cad059e82ae4a229b4ea52cb03398bad
SandrinePichla <sandrinepichla@hotmail.com>
2026-02-26 19:52:00 +0100

    modif du schema pour avoir le bouton add item

--- schemaTypes/discipline.js
+++ schemaTypes/discipline.js
@@ -40,49 +40,25 @@ export default {
     },
     {
       // Array de strings avec liste prédéfinie + "Add item" pour saisie libre
-      name: 'professeurs',
-      title: 'Professeurs',
-      type: 'array',
-      of: [{ type: 'string' }],
-      options: {
-        list: [
-          { title: 'Stéphanie Lamoureux', value: 'Stéphanie Lamoureux' },
-          { title: 'Myriam Reuter', value: 'Myriam Reuter' },
-          { title: 'Alexandre Houze', value: 'Alexandre Houze' },
-          { title: 'Jérémie Sigalat', value: 'Jérémie Sigalat' },
-          { title: 'Sylvaine Colas', value: 'Sylvaine Colas' },
-        ]
-      }
-    },
-    {
-      name: 'niveaux',
-      title: 'Niveaux acceptés',
-      type: 'array',
-      of: [{ type: 'string' }],
-      options: {
-        list: [
-          { title: 'Débutant', value: 'Débutant' },
-          { title: 'Intermédiaire', value: 'Intermédiaire' },
-          { title: 'Expert', value: 'Expert' },
-          { title: 'Tous niveaux', value: 'Tous niveaux' }
-        ]
-      }
-    },
-    {
-      name: 'ages',
-      title: 'Âges acceptés',
-      type: 'array',
-      of: [{ type: 'string' }],
-      options: {
-        list: [
-          { title: '6/8 ans', value: '6-8 ans' },
-          { title: '9/12 ans', value: '9-12 ans' },
-          { title: '13 ans et +', value: '13 ans et +' },
-          { title: '14 ans et +', value: '14 ans et +' },
-          { title: 'Adultes', value: 'Adultes' },
-        ]
-      }
-    },
+
+  name: 'professeurs',
+  title: 'Professeurs',
+  type: 'array',
+  of: [{ type: 'string' }],
+  // Pas de list prédéfinie = bouton "Add item" + saisie libre totale
+},
+{
+  name: 'niveaux',
+  title: 'Niveaux acceptés',
+  type: 'array',
+  of: [{ type: 'string' }],
+},
+{
+  name: 'ages',
+  title: 'Âges acceptés',
+  type: 'array',
+  of: [{ type: 'string' }],
+},
     {
       name: 'ordre',
       title: "Ordre d'affichage",
