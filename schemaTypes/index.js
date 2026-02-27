8d64c2edf3f5b906c311982584bab26d0c471e41
SandrinePichla <sandrinepichla@hotmail.com>
2026-02-27 22:05:21 +0100

    Tarifs speciaux

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -2,6 +2,7 @@ import actualite from './actualite'
 import discipline from './discipline'
 import cours from './cours'
 import tarif from './tarif'
+import tarifSpecial from './tarifSpecial'
 import galerie from './galerie'
 
-export const schemaTypes = [actualite, discipline, cours, tarif, galerie]
+export const schemaTypes = [actualite, discipline, cours, tarif, tarifSpecial, galerie]
